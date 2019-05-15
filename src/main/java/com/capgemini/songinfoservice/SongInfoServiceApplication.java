9e06ca83485567ced4d8f69484c17d451f23536e
pawar <kapil.pawar@capgemini.com>
2019-05-15 17:24:36 +0530

    added eureka server

--- src/main/java/com/capgemini/songinfoservice/SongInfoServiceApplication.java
+++ src/main/java/com/capgemini/songinfoservice/SongInfoServiceApplication.java
@@ -2,8 +2,13 @@ package com.capgemini.songinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SongInfoServiceApplication {
 
 	public static void main(String[] args) {
